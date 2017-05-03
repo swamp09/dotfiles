d2b75c09b1bab6cac34cf60385d045765ceb246d
swamp09 <shu.numata@esm.co.jp>
2017-05-03 23:15:31 +0900

    Modify .gitconfig

--- dotfilesInstaller.sh
+++
@@ -1,8 +0,0 @@
- ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.bundle ~/.bundle
-ln -s ~/dotfiles/.gem ~/.gem
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/.rbenv ~/.rbenv
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
