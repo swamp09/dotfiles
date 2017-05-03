e57fd039ad0ba5c57c60ed34c7549eae36b2cc8d
swamp09 <shu.numata@esm.co.jp>
2017-05-03 23:14:25 +0900

    Modify Installer

---
+++ installer.sh
@@ -0,0 +1,6 @@
+ ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
