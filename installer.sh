8dc2a1cf415d8479ddf6c95d6621af64a08c9a03
swamp09 <shu.numata@esm.co.jp>
2018-03-16 14:20:58 +0900

    Remove rubocop.yml setting

--- installer.sh
+++ installer.sh
@@ -3,7 +3,6 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 ln -s ~/dotfiles/.emacs.d ~/.emacs.d
 ln -s ~/dotfiles/.pryrc ~/.pryrc
 ln -s ~/dotfiles/.irbrc ~/.irbrc
