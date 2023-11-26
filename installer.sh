6a0f912a254b6ad837347e5b9545252ea0b83894
swamp09 <shu.numata@esm.co.jp>
2023-11-26 15:32:41 +0900

    Remove unused config

--- installer.sh
+++ installer.sh
@@ -1,9 +1,4 @@
- ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
-ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.pryrc ~/.pryrc
-ln -s ~/dotfiles/.irbrc ~/.irbrc
 ln -s ~/dotfiles/.gemrc ~/.gemrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
